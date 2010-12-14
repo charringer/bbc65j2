ee2da702f87cebf91147c19354b809ec4a339ce9
Gabriel Ebner <ge@gabrielebner.at>
2010-12-14 17:54:06 +0100

    Test: run unit tests.

--- Test.java
+++ Test.java
@@ -2,7 +2,11 @@
 public class Test {
 
 	public static void main(String[] args) {
-
+		TestRunner.main(
+				BluRayDriveTest.class,
+				CardReaderTest.class,
+				PortTest.class,
+				RootTest.class);	
 	}
 
 }
